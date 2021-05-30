28a75a4d1a6a44b90a5a9d770d788a2f35f2463d
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-05-30 19:25:09 -0300

    added xml accepted header

--- api/Serializer.js
+++ api/Serializer.js
@@ -1,13 +1,30 @@
 const ValueNotSupported = require("./error/ValueNotSupported");
-
+const jsontoxml = require("jsontoxml");
 class Serializer {
   json(data) {
     return JSON.stringify(data);
   }
 
+  xml(data) {
+    let tag = this.tagSingleSupplier;
+    if (Array.isArray(data)) {
+      tag = this.tagMoreThanOneSupplier;
+      data = data.map((item) => {
+        return {
+          [this.tagSingleSupplier]: item,
+        };
+      });
+    }
+    return jsontoxml({ [tag]: data });
+  }
+
   serialize(data) {
+    data = this.filter(data);
     if (this.contentType === "application/json") {
-      return this.json(this.filter(data));
+      return this.json(data);
+    }
+    if (this.contentType === "application/xml") {
+      return this.xml(data);
     }
 
     throw new ValueNotSupported(this.contentType);
@@ -40,6 +57,8 @@ class SerializerSuppliers extends Serializer {
     super();
     this.contentType = contentType;
     this.publicFields = ["id", "company", "category"].concat(extraFields || []);
+    this.tagSingleSupplier = "supplier";
+    this.tagMoreThanOneSupplier = "suppliers";
   }
 }
 
@@ -48,6 +67,8 @@ class SerializerErrors extends Serializer {
     super();
     this.contentType = contentType;
     this.publicFields = ["id", "message"].concat(extraFields || []);
+    this.tagSingleSupplier = "error";
+    this.tagMoreThanOneSupplier = "errors";
   }
 }
 
@@ -55,5 +76,5 @@ module.exports = {
   Serializer,
   SerializerSuppliers,
   SerializerErrors,
-  acceptedFormats: ["application/json"],
+  acceptedFormats: ["application/json", "application/xml"],
 };
